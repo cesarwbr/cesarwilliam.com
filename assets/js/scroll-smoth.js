43a95f2dd1c0bfaa90e8235f61ed4c92305a498d
Cesar William <cesarwbr@gmail.com>
2016-09-03 16:47:17 -0500

    Update 2016-09-03T21:47:16.383Z

--- assets/js/scroll-smoth.js
+++ assets/js/scroll-smoth.js
@@ -4,10 +4,9 @@ $(function () {
         if (location.pathname.replace(/^\//, '') === this.pathname
             .replace(/^\//, '') && location.hostname === this.hostname
         ) {
-            let target = $(this.hash);
+            var target = $(this.hash);
             target = target.length ? target : $(
-                `[name=${this
-                .hash.slice(1)}]`
+                '[name=' + this.hash.slice(1) + ']'
             );
             if (target.length) {
                 $('html,body').animate({
